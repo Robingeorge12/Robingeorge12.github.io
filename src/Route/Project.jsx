3a89ee8798d4ba3df44b7bf7b31d7127b2ce4f1e
Robingeorge12 <robingsa.btch@gmail.com>
2022-11-27 19:13:01 +0530

    pf

--- src/Route/Project.jsx
+++ src/Route/Project.jsx
@@ -17,21 +17,21 @@ export default function Project() {
                 <Box>
 
                     <VStack>
-                        <Box h="400px" w={["280px","320","350px"]} spacing="40px" bgColor="#060c30">
-                            <Image width="100%" height="150px" src="https://i.ibb.co/pQBzxRV/Home.png" alt="prpic" />
+                        <Box h="400px" w={["280px", "320", "350px"]} spacing="40px" bgColor="#060c30">
+                            <Image width="100%" height="150px" src="https://i.ibb.co/MMzhb1P/better.png" alt="prpic" />
                             <Box>
-                                <Text textColor="red.300">INDIAMART.COM</Text>
-                                <Box w={["250px","260px","320px"]} ml="12px" pb="10px">
-                                <Text align="left" textColor="white">
-                                    IndiaMart.com is known for its unparalleled assortment of pharmaceuticals and cosmetics products. </Text>
-                                <Text align="left" textColor="white">  A collaborative project built by a team of 5,executed in 5 days</Text>
+                                <Text textColor="red.300">BETTER MART.COM</Text>
+                                <Box w={["250px", "260px", "320px"]} ml="12px" pb="10px">
+                                    <Text align="left" textColor="white">
+                                    Better Mart is a clone of an E-Commerce website that mainly sells household items. </Text>
+                                    <Text align="left" textColor="white">A collaborative project built by a team of 5, executed in 5 days.</Text>
                                 </Box>
-                                <Text textColor="white">* HTML | CSS | JAVASCRIPTS</Text>
+                                <Text textColor="white">* MONGO DB | EXPRESS | NODE JS | REACT | REDUX | CHAKRA UI </Text>
 
                             </Box>
                             <HStack>
-                               <Box ml="60px" mr={4}><Link href="https://github.com/Avdhoot-Kurhe/indianMART_Site"><Button bgColor="red.300" color="white">CODE <Text pl={4}><BsGithub /></Text> </Button></Link></Box> 
-                               <Box mr="-20px"> <Link href="https://idyllic-elf-19b91b.netlify.app/"><Button bgColor="red.300" color="white">LIVE <Text pl={4}><GoLinkExternal /></Text></Button></Link></Box> 
+                                <Box ml="60px" mr={4}><Link href="https://github.com/Avdhoot-Kurhe/indianMART_Site"><Button bgColor="red.300" color="white">CODE <Text pl={4}><BsGithub /></Text> </Button></Link></Box>
+                                <Box mr="-20px"> <Link href="https://idyllic-elf-19b91b.netlify.app/"><Button bgColor="red.300" color="white">LIVE <Text pl={4}><GoLinkExternal /></Text></Button></Link></Box>
 
                             </HStack>
                         </Box>
@@ -39,40 +39,54 @@ export default function Project() {
                 </Box>
                 <Box >
                     <VStack>
-                        <Box bgColor="#060c30" h="400px" w={["280px","320","350px"]} spacing="40px">
+                        <Box bgColor="#060c30" h="400px" w={["280px", "320", "350px"]} spacing="40px">
                             <Image width="100%" height="150px" src="https://i.ibb.co/hLhppT4/Screenshot-new.png" alt="pr4" />
                             <Box>
                                 <Text textColor="red.300">SHINE.COM</Text>
-                                <Box w={["250px","260px","320px"]} ml="12px" pb="10px">
-                                <Text align="left" textColor="white">Shine.com is a job career guidace and job searching website</Text>
-                                <Text textColor="white"align="left">An individual mini project ,completed in 4 days</Text>
+                                <Box w={["250px", "260px", "320px"]} ml="12px" pb="10px">
+                                    <Text align="left" textColor="white">Shine.com is a job career guidace and job searching website</Text>
+                                    <Text textColor="white" align="left">An individual mini project, completed in 4 days.</Text>
                                 </Box>
                                 <Text mt="25px" textColor="white">*  HTML | CSS3 | REACT JS </Text>
-                                
+
                             </Box>
                             <HStack>
-                            <Box ml="60px" mr={4}><Link href="https://github.com/Robingeorge12/capable-downtown-7255"><Button bgColor="red.300" color="white">CODE <Text pl={4}><BsGithub /></Text> </Button></Link></Box>
-                            <Box mr="60px" ><Link href="https://capable-downtown-7255.vercel.app/"><Button bgColor="red.300" color="white">LIVE <Text pl={4}><GoLinkExternal /></Text></Button></Link></Box>
+                                <Box ml="60px" mr={4}><Link href="https://github.com/Robingeorge12/capable-downtown-7255"><Button bgColor="red.300" color="white">CODE <Text pl={4}><BsGithub /></Text> </Button></Link></Box>
+                                <Box mr="60px" ><Link href="https://capable-downtown-7255.vercel.app/"><Button bgColor="red.300" color="white">LIVE <Text pl={4}><GoLinkExternal /></Text></Button></Link></Box>
                             </HStack>
                         </Box>
                     </VStack>
                 </Box>
                 <Box>
                     <VStack>
-                        <Box bgColor="#060c30" h="400px" w={["280px","320","350px"]} spacing="40px">
+                        <Box bgColor="#060c30" h="400px" w={["280px", "320", "350px"]} spacing="40px">
                             <Image width="100%" height="150px" src="https://i.ibb.co/WsyHVHh/Screenshot-116.png" alt="pr5" />
-                            <Box >
+
+                            <Box>
+                                <Text textColor="red.300">INDIAMART.COM</Text>
+                                <Box w={["250px", "260px", "320px"]} ml="12px" pb="10px">
+                                    <Text align="left" textColor="white">
+                                        IndiaMart.com is known for its unparalleled assortment of pharmaceuticals and cosmetics products. </Text>
+                                    <Text align="left" textColor="white">A collaborative project built by a team of 5, executed in 5 days.</Text>
+                                </Box>
+                                <Text textColor="white">* HTML | CSS | JAVASCRIPTS</Text>
+
+                            </Box>
+
+
+
+                            {/* <Box >
                                 <Text textColor="red.300">GEAR-BEST.COM</Text>
-                                <Box w={["250px","260px","320px"]} ml="12px" pb="10px">
-                                <Text align="left" textColor="white"> Gear-Best.com is best in selling gadgets and hardwares. </Text>
-                                <Text align="left" textColor="white">It was a collaborative project of 6 members, completed in 5 days</Text>
+                                <Box w={["250px", "260px", "320px"]} ml="12px" pb="10px">
+                                    <Text align="left" textColor="white"> Gear-Best.com is best in selling gadgets and hardwares. </Text>
+                                    <Text align="left" textColor="white">It was a collaborative project of 6 members, completed in 5 days</Text>
                                 </Box>
                                 <Text mt="25px" textColor="white">* HTML | CSS3 | JAVASCRIPT </Text>
-                            </Box>
+                            </Box> */}
                             <HStack>
-{/* //https://gearbest-iflh.vercel.app/" */}
-                            <Box ml="70px" mr={4}> <Link href="https://github.com/Lalitsingh28/GEARBEST_CLONE"><Button bgColor="red.300" color="white" >CODE <Text pl={2}><BsGithub /></Text> </Button></Link></Box>
-                            <Box mr="60px" ><Link href="" ><Button bgColor="red.300" color="white" >LIVE <Text pl={2}><GoLinkExternal /></Text></Button></Link></Box>
+
+                                <Box ml="70px" mr={4}> <Link href="https://github.com/Lalitsingh28/GEARBEST_CLONE"><Button bgColor="red.300" color="white" >CODE <Text pl={2}><BsGithub /></Text> </Button></Link></Box>
+                                <Box mr="60px" ><Link href="https://gearbest-iflh.vercel.app/" ><Button bgColor="red.300" color="white" >LIVE <Text pl={2}><GoLinkExternal /></Text></Button></Link></Box>
                             </HStack>
                         </Box>
                     </VStack>
